9ee11fbcf2b04574d0aecc10fd2ff9b5cbbce767
shekhargrace14 <shekhargrace14@gmail.com>
2023-10-13 23:03:29 +0530

    slider done

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let flag = 0;
+let flag = 1;
+
+function controller(x){
+    flag = flag + x;
+    slideshow(flag)
+}
 
 slideshow(flag);
 
 function slideshow(num){
     let slides = document.getElementsByClassName('slide')
 
-    slides[num].style.display = "block";
+    if(num == slides.length){
+        flag=0;
+        num= 0;
+    }
 
-}
+    for (let y of slides){
+        y.style.display = "none"
+    }
+
+    slides[num].style.display = "block"; 
+
+}   
